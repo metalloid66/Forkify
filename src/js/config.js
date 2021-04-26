0903f8fdc1f1e559e714472e090db9977cd358fd
metalloid66 <mohamadnabeel3@gmail.com>
2021-04-26 12:47:28 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '07e5a7b3-28f9-43c4-8d0e-bdfb5033525c';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,12 @@
+import View from './View.js';
+import previewView from './previewView.js'
+class ResultsView extends View{
+ _parentElement = document.querySelector('.results')
+ _errorMessage = 'No recipes Found for your Query! Please try again ';
+ _message = ``;
+ _generateMarkup(){
+    return this._data.map(result => previewView.render(result,false)).join('')
+
+ }
+}
+export default new ResultsView();
